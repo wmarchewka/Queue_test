ec9423887d8c4134cafe12e495b5b65198557ec9
Walter Marchewka <walter.marchewka@siemens.com>
2023-05-23 12:32:45 -0400

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ static const char *TAG = "main";
 
 struct message
 {
-    std::string rx_buffer;
+    int counter;
+    char data[128];
 };
 
 struct commander_parameters
@@ -35,21 +36,21 @@ extern "C" void app_main()
 
     QueueHandle_t xUdpQueue = xQueueCreate(20, sizeof(message));
 
-    udp_server_parameters udp_parameters = {
+    udp_server_parameters udp_parm = {
         .xUdpQueue = xUdpQueue,
     };
 
-    commander_parameters commander_parameters = {
+    commander_parameters commander_parm = {
         .xUdpQueue = xUdpQueue,
     };
 
-    xReturned = xTaskCreate(commander_task, "commander_task", 1024 * 8, &commander_parameters, 11, NULL);
+    xReturned = xTaskCreate(commander_task, "commander_task", 1024 * 16, &commander_parm, 11, NULL);
     if (xReturned != pdPASS)
     {
         ESP_LOGE(TAG, "Failed to create Commander Task");
     }
 
-    xReturned = xTaskCreate(udp_server_task, "udp_task", 1024 * 8, &udp_parameters, 12, NULL);
+    xReturned = xTaskCreate(udp_server_task, "udp_task", 1024 * 16, &udp_parm, 11, NULL);
     if (xReturned != pdPASS)
     {
         ESP_LOGE(TAG, "Failed to create UDP Server Task");
@@ -61,10 +62,10 @@ extern "C" void app_main()
 void udp_server_task(void *pvParameters)
 {
     // ESP_LOGI(TAG, "UDP Server Task starting up...");
-    QueueHandle_t xUdpQueue;
-    char rx_buffer[128];
+    QueueHandle_t x_TestQueue;
+    char rx_buffer[128]={};
     message udp_message;
-    int ret;
+
 
     udp_server_parameters *pTaskParameters = (udp_server_parameters *)pvParameters;
     if (NULL == pTaskParameters ||
@@ -75,7 +76,7 @@ void udp_server_task(void *pvParameters)
     }
     // ESP_LOGE(TAG, "UDP Task parameters were OK");
 
-    xUdpQueue = pTaskParameters->xUdpQueue;
+    x_TestQueue = pTaskParameters->xUdpQueue;
 
     // ESP_LOGE(TAG, "UDP OK");
 
@@ -83,11 +84,14 @@ void udp_server_task(void *pvParameters)
     {
         for (int x = 1; x < 10; x++)
         {
-            sprintf(rx_buffer, "%i", x);
-            udp_message.rx_buffer = rx_buffer;
-            printf("Write queue:%s\n", udp_message.rx_buffer.c_str());
-            ret = xQueueSend(xUdpQueue, &udp_message, pdMS_TO_TICKS(0));
-            vTaskDelay(pdMS_TO_TICKS(250));
+            sprintf(rx_buffer, "Text data:%i", x);
+            printf("%s\n", rx_buffer);
+            udp_message.counter = x;
+            memcpy(udp_message.data, rx_buffer,  strlen(rx_buffer));
+            printf("Write int:%i\n", udp_message.counter);
+            printf("Write char:%s\n", udp_message.data);
+            xQueueSend(x_TestQueue, &udp_message, pdMS_TO_TICKS(0));
+            vTaskDelay(pdMS_TO_TICKS(500));
         }
     }
 }
@@ -96,7 +100,7 @@ void commander_task(void *pvParameters)
 {
     // ESP_LOGD(TAG, "Commander Task starting...");
     message commander_message;
-    QueueHandle_t xUdpQueue;
+    QueueHandle_t x_TestQueue;
 
     commander_parameters *pTaskParameters = (commander_parameters *)pvParameters;
     if (NULL == pTaskParameters ||
@@ -108,20 +112,17 @@ void commander_task(void *pvParameters)
 
     // ESP_LOGE(TAG, "Task parameters were OK.");
 
-    xUdpQueue = pTaskParameters->xUdpQueue;
+    x_TestQueue = pTaskParameters->xUdpQueue;
 
     // ESP_LOGE(TAG, "Got parameters");
 
     while (true)
     {
-        if (xQueueReceive(xUdpQueue, &commander_message, portMAX_DELAY))
+        if (xQueueReceive(x_TestQueue, &commander_message, portMAX_DELAY))
         {
-            printf("Recv Buffer:%s\n", commander_message.rx_buffer.c_str());
-            vTaskDelay(pdMS_TO_TICKS(500));
-        }
-        else
-        {
-            printf("ERROR\n");
+            printf("Recv Buffer int:%i\n", commander_message.counter);
+            printf("Recv Buffer str:%s\n", commander_message.data);
+            vTaskDelay(pdMS_TO_TICKS(1000));
         }
     }
 }
